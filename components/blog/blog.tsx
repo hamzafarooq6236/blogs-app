a2d4110a66e1fbd0419c1e4a1b239117d3e16840
Hamza Farooq <hamzafarooq109@gmail.com>
2026-08-11 17:29:45 +0500

    underline and make link blue when hover over it

--- components/blog/blog.tsx
+++ components/blog/blog.tsx
@@ -10,7 +10,7 @@ interface BlogProps{
 export default function blog({title,content,slug}:BlogProps) {
     return (
         <div className="flex items-center border-2 w-[65vw] rounded-2xl p-2">
-            <Link href={`/blogs/${slug}`}>{title}</Link>
+            <Link href={`/blogs/${slug}`}className="hover:text-blue-400 hover:underline hover:underline-offset-3">{title}</Link>
             <Pencil className="text-black ml-auto h-4 "/>
         </div>
     )
