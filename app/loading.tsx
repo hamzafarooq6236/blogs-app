abe0ccab5fb5f413e51820cf9df9c5fc56a61f24
Hamza Farooq <hamzafarooq109@gmail.com>
2026-08-17 12:33:45 +0500

    make the skeleton loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,17 +2,20 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
     return (
-        <div className="w-full min-h-screen max-w-md p-4 border border-gray-200 rounded-md shadow animate-pulse">
-            {/* Circle Placeholder (Avatar) */}
-            <div className="flex space-x-4">
-                <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-                <div className="flex-1 py-1 space-y-4">
-                    {/* Title Line Placeholder */}
-                    <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-                    <div className="space-y-2">
-                        {/* Body Line Placeholders */}
-                        <div className="h-4 bg-gray-300 rounded"></div>
-                        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+        <div className="w-full min-h-screen bg-[#E3FDFD] p-8 sm:px-6 lg:px-8">
+            <div className="max-w-5xl mx-auto w-full p-6 bg-white/40 border border-[#CBF1F5] rounded-3xl shadow-sm animate-pulse">
+                {/* Circle Placeholder (Avatar) */}
+                <div className="flex space-x-6">
+                    <div className="w-12 h-12 bg-[#A6E3E9] rounded-2xl"></div>
+                    <div className="flex-1 py-1 space-y-5">
+                        {/* Title Line Placeholder */}
+                        <div className="h-6 bg-[#A6E3E9] rounded-md w-1/3"></div>
+                        <div className="space-y-3">
+                            {/* Body Line Placeholders */}
+                            <div className="h-4 bg-[#CBF1F5] rounded w-full"></div>
+                            <div className="h-4 bg-[#CBF1F5] rounded w-5/6"></div>
+                            <div className="h-4 bg-[#CBF1F5] rounded w-4/6"></div>
+                        </div>
                     </div>
                 </div>
             </div>
